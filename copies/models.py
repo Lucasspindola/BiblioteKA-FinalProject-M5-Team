807415a7e74de625622a16400ea2679c3972b572
Lucas <lucasspindolafernandes196@hotmail.com>
2023-03-07 11:31:00 -0300

    feat/add: CopieLoan pivot table

--- copies/models.py
+++ copies/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+import datetime
 # Create your models here.
+
+class CopieLoan(models.Model):
+    class Meta:
+        ordering = ("id",)
+
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    copie = models.ForeignKey("copies.Copie", on_delete=models.CASCADE)
+    loan_date = models.DateTimeField(default=datetime.datetime.now())
+    return_date = models.DateTimeField(null=True, blank=True)
+    expected_return_date = models.DateTimeField(null=True, blank=True)
