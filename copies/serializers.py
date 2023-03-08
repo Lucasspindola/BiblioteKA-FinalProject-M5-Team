47aa01d962e84bca2f071eb51702f9096cf92757
Jalles <jallessantos12@gmail.com>
2023-03-08 10:54:53 -0300

    feat: add loan url

--- copies/serializers.py
+++ copies/serializers.py
@@ -28,24 +28,6 @@ class LoanSerializer(serializers.ModelSerializer):
         user_id = validated_data["user_id"]
         user = get_object_or_404(User, id=user_id)
         current_day = datetime.now()
-        # Cada livro só poderá ser emprestado por um período fixo de tempo
-        # Deverá ser criada uma lógica onde, se a devolução cair em um fim de semana (sábado ou domingo),
-        # a data de retorno deverá ser modificada para ser no próximo dia útil
-        max_loan_period = timedelta(days=7)
-        expected_return = current_day + max_loan_period
-        if expected_return.weekday() >= 5:  # sábado é 5 e domingo é 6
-            expected_return = expected_return + timedelta(
-                days=(7 - expected_return.weekday())
-            )
-
-        # Todos os livros emprestados deverão ter uma data de retorno
-
-        # Caso o estudante não devolva o livro até o prazo estipulado,
-        # deverá ser impedido (bloqueado) de solicitar outros empréstimos.
-
-        # Se um estudante não efetuar a devolução dos livros no prazo estipulado,
-        # ele não poderá emprestar mais livros até completar a devolução dos anteriores.
-        # Após completar as devoluções pendentes, o bloqueio deve permanecer por alguns dias.
         return ""
 
     def get_expected_return_date(self, obj):
@@ -70,4 +52,4 @@ class LoanSerializer(serializers.ModelSerializer):
             "expected_return_date",
             "delivery_date",
         ]
-        read_only_fields = ["loan_date", "delivery_date"]
+        read_only_fields = ["loan_date", "delivery_date", "copie"]
