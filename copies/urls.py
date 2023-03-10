2bef808b28eb68dcb77b458388b4a6c18fc0fa40
Jalles <jallessantos12@gmail.com>
2023-03-10 13:55:24 -0300

    feat: add LoanHistoryView and CustomLoanHistoryPermission, and alter books/loans/history/<user_id>/ permissions

--- copies/urls.py
+++ copies/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import CopieView, LoansView, LoanDetailView
+from .views import CopieView, LoansView, LoanDetailView, LoanHistoryView
 
 
 urlpatterns = [
     path("copies/<int:book_id>/", CopieView.as_view()),
-    path("books/loans/<int:pk>/", LoansView.as_view()),
-    path("books/loans/devolution/<int:pk>/", LoanDetailView.as_view()),
-    path("books/loans/history/<int:pk>/", LoansView.as_view()),
+    path("books/loans/<int:pk>/", LoansView.as_view()),  # adm
+    path("books/loans/devolution/<int:pk>/", LoanDetailView.as_view()),  # adm
+    path("books/loans/history/<int:user_id>/", LoanHistoryView.as_view()),
 ]
